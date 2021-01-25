b8589ad51d0f2413e84de5a69de00d841891d25f
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-25 17:21:54 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -14,5 +14,12 @@ suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
 
-
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
 
